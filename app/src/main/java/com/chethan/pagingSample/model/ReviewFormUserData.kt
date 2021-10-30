d94b8209140369850a5b81ff7cb1f975f42acf7e
Chethan <chethu002@gmail.com>
2021-10-30 14:48:32 +0530

    Changes : Refactoring Code

--- app/src/main/java/com/chethan/pagingSample/model/ReviewFormUserData.kt
+++ app/src/main/java/com/chethan/pagingSample/model/ReviewFormUserData.kt
@@ -1,19 +1,6 @@
 package com.chethan.pagingSample.model
 
-import android.net.Uri
-
 // Single source of truth to collect all the user interaction on UI
 data class ReviewFormUserData(
-    var nickName: String = "",
-    var comment: String = "",
-    var ageGroup: String = "",
-    var ageGroupEnumName: String = "",
-    var travelType: String = "",
-    var travelTypeEnumName: String = "",
-    var evaluationTotalScore: String = "",
     var mediaUploadInformation: HashMap<String, UploadedItem> = HashMap(),
-    var evaluationScore: HashMap<String, String> = HashMap(),
-    var postReviewTermsAccepted: Boolean? = false
-) {
-
-}
+)
